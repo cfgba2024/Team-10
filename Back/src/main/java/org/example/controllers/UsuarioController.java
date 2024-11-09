7ab543b06600478c08271f92f25798b6c98abf97
Alan <159034743+AlanLeandroVargas@users.noreply.github.com>
2024-11-09 15:31:22 -0300

    Configuracion de Spring

--- Back/src/main/java/org/example/controllers/UsuarioController.java
+++ Back/src/main/java/org/example/controllers/UsuarioController.java
@@ -1,4 +1,11 @@
 package org.example.controllers;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/api/users")
 public class UsuarioController {
+    @GetMapping
+    public String getAllUsers() {
+        return "Test users";  // Returns the list of users
+    }
 }
